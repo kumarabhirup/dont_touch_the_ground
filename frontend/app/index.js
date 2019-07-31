5fb17f2b416a0b6c899cc8ae0a484275f6673d05
Kumar Abhirup <kumar@iqubex.com>
2019-07-31 18:40:55 +0530

    👌 IMPROVE: add initial variables

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,5 +1,49 @@
 /* global Koji, createCanvas, background, textSize, text */
 
+let myFont // The font we'll use throughout the app
+
+let gameOver = false // If it's true the game will render the main menu
+let gameBeginning = true // Should be true only before the user starts the game for the first time
+
+// Game objects
+// Declare game objects here like player, enemies etc
+
+// Buttons
+let playButton
+let soundButton
+
+// Score data
+let score = 0
+let highScore = 0
+let highscoreGained = false
+let scoreGain
+
+// Data taken from Game Settings
+let startingLives
+let lives
+
+// Images
+let imgLife
+let imgBackground
+
+// Audio
+let sndMusic
+let soundEnabled = true
+let canMute = true
+
+let soundImage
+let muteImage
+
+// Size stuff
+let objSize //base size modifier of all objects, calculated based on screen size
+
+// game size in tiles, using bigger numbers will decrease individual object sizes but allow more objects to fit the screen
+// Keep in mind that if you change this, you might need to change text sizes as well
+let gameSize = 18
+
+let isMobile = false
+let touching = false // Whether the user is currently touching/clicking
+
 // Load assets
 function preload() {
 
